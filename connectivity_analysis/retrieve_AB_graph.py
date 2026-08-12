64b8c5b3c3138796851f9d8e62b719626a7b70c4
aholtrup <166074064+aholtrup@users.noreply.github.com>
2026-08-12 17:20:58 +0200

    Add files via upload

---
+++ connectivity_analysis/retrieve_AB_graph.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import networkx as nx
+
+
+### load graphs
+G_AB_con = nx.read_graphml("htem/PF_PC/connectivity_analysis/graphs/G_AB_con.graphml") 
+G_AB_touch = nx.read_graphml("htem/PF_PC/connectivity_analysis/graphs/G_AB_con.graphml") 
+
+
+### extract cell types
+node_types = nx.get_node_attributes(G_con, "type")
+
+PC_set  = [n for n, t in node_types.items() if t == "pc"]
+gPF_set  = [n for n, t in node_types.items() if t == "gpf"] #parallel fibers of local granule cells
+AB_set = [n for n, t in node_types.items() if t == "local_grc"] #ascending branches of local granule cells
